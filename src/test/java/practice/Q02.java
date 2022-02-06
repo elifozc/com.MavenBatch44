9a0452813c553e5c0c0b7f2c4c558fed39cc276e
elifozc <elfnrsna@gmail.com>
2022-02-06 19:03:36 +0100

    Initial commit

---
+++ src/test/java/practice/Q02.java
@@ -0,0 +1,15 @@
+package practice;
+
+public class Q02 {
+
+     /*
+...Exercise2...
+   http://www.bestbuy.com 'a gidin,
+   Sayfa basliginin "Best" icerdigini(contains) dogrulayin
+   Ayrica Relative Locator kullanarak;
+       logoTest => BestBuy logosunun gorunutulenip goruntulenmedigini dogrulayin
+       mexicoLinkTest => Linkin gorunutulenip goruntulenmedigini dogrulayin
+
+*/
+
+}
